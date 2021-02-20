97bba5c4b935a501d416d92c15c515e9c4f77fac
Omochin_baseus <43504088+GenSSK@users.noreply.github.com>
2021-02-20 20:23:35 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class read:
+    def print_hi(self, name):
+        # Use a breakpoint in the code line below to debug your script.
+        print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    def read_csv(self):
+        self.data = pd.read_csv('DATA.csv')
+
+    def graph(self):
+        plt.rcParams['font.family'] = 'Times New Roman'
+        plt.rcParams['mathtext.default'] = 'regular'
+        plt.rcParams['xtick.top'] = 'True'
+        plt.rcParams['ytick.right'] = 'True'
+        # plt.rcParams['axes.grid'] = 'True'
+        plt.rcParams['xtick.direction'] = 'in'  # x軸の目盛線が内向き('in')か外向き('out')か双方向か('inout')
+        plt.rcParams['ytick.direction'] = 'in'  # y軸の目盛線が内向き('in')か外向き('out')か双方向か('inout')
+        plt.rcParams['xtick.major.width'] = 1.0  # x軸主目盛り線の線幅
+        plt.rcParams['ytick.major.width'] = 1.0  # y軸主目盛り線の線幅
+        plt.rcParams['font.size'] = 12  # フォントの大きさ
+        plt.rcParams['axes.linewidth'] = 1.0  # 軸の線幅edge linewidth。囲みの太さ
+
+        plt.rcParams["legend.fancybox"] = False  # 丸角
+        plt.rcParams["legend.framealpha"] = 0  # 透明度の指定、0で塗りつぶしなし
+        plt.rcParams["legend.edgecolor"] = 'black'  # edgeの色を変更
+        plt.rcParams["legend.handlelength"] = 2  # 凡例の線の長さを調節
+        plt.rcParams["legend.labelspacing"] = 0.1  # 垂直方向（縦）の距離の各凡例の距離
+        plt.rcParams["legend.handletextpad"] = .4  # 凡例の線と文字の距離の長さ
+
+        plt.rcParams["legend.markerscale"] = 2  # 点がある場合のmarker scale
+        plt.rcParams['axes.xmargin'] = '0'  # '.05'
+        plt.rcParams['axes.ymargin'] = '0'
+        plt.rcParams['savefig.facecolor'] = 'None'
+        plt.rcParams['savefig.edgecolor'] = 'None'
+        # plt.rcParams['savefig.bbox'] = 'tight'
+        plt.rcParams['pdf.fonttype'] = 42  # PDFにフォントを埋め込むためのパラメータ
+
+        plt.figure(figsize=(20, 10), dpi=100)
+
+        # plt.scatter(self.data['Time'], self.data['wm'])
+        plt.plot(self.data['Time'], self.data['wm'])
+        plt.xlim(2, 3)  # x軸の範囲
+        plt.xlabel('Time[sec]')
+        plt.ylabel('Velocity[rad/s]')
+        plt.show()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    rd = read()
+    # rd.print_hi('PyCharm')
+    rd.read_csv()
+    rd.graph()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readCSV.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import glob
+
+class readCSV:
+    def __init__(self):
+
+    def csv_read(self):
+        # CSV読み込むよ！
+        self.data = pd.read_csv('DATA.csv')
