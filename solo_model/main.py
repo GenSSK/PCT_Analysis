b2ab75f77ab7a7e07654103f5ec4efcd30cc70d7
Omochin_baseus <43504088+GenSSK@users.noreply.github.com>
2022-04-10 01:04:39 +0900

    update

--- solo_model/main.py
+++ solo_model/main.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
     npz = Npz.NPZ()
     data = npz.single_load('g.sasaki_ln5_id30_h130_h250_h327_od4_lr0_bn3000_en1000.npz')
     # data = npz.single_load('2022-04-08_5lay_mini_epo1000_1_ACTUAL.npz')
-    # data_force = npz.single_load('2022-04-08_force_5lay_epo100_1.npz')
-    # data_force = npz.single_load('2022-04-08_force_5lay_epo100_1_ACTUAL.npz')
+    data_force = npz.single_load('g.sasaki_ln5_id30_h130_h250_h325_od2_lr0.000100_bn3000_en500_force.npz')
+    # data_force = npz.single_load('g.sasaki_ln5_id30_h130_h250_h325_od2_lr0_bn3000_en500_force_ACTUAL.npz')
     pct = PCT.PCT()
     sm = solomodel.SoloModel(data)
     hist = histogram.histogram()
@@ -27,14 +27,14 @@ if __name__ == '__main__':
 
     # pct.task_show(data)
 
-    sm.check_loss()
+    # sm.check_loss()
     # sm.recalc_ball_movement()
-    sm.check_ball()
-    sm.analyze()
+    # sm.check_ball()
+    # sm.analyze()
 
-    # sm_force = solomodel.SoloModel(data_force)
-    # sm_force.check_loss()
-    # sm_force.analyze_force()
+    sm_force = solomodel.SoloModel(data_force)
+    sm_force.check_loss()
+    sm_force.analyze_force()
 
 
     # hist.graph_out(data)
